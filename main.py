63fdcda7fb0e79eade0f2b3c53cabd654ed6ab37
ShunsukeTamaki <tamazoo0913@gmail.com>
2023-11-21 00:44:40 +0900

    update/main.py リファクタ&キャラウィンドウにビット反転処理の追加

--- main.py
+++ main.py
@@ -1,109 +1,117 @@
-
 import os
 import cv2
 import pytesseract
 import numpy as np
 
 
-def CaptureArea(video):
+def Init() -> None:
+    """インストール済みのTesseractのパスを通す
+    """
+    
+    TESSERACT_PATH = "C:\Program Files\Tesseract-OCR"
+    if TESSERACT_PATH not in os.environ["PATH"].split(os.pathsep):
+        os.environ["PATH"] += TESSERACT_PATH
 
-    # トリミングのために2秒進める
-    for i in range(120):
 
-        # フレームを取得する
-        ret, frame = video.read(i)  # これを呼び出すことで動画が1フレーム進んでいそう？
+def CaptureArea(frame):
+    """指定した領域の座標と幅と高さを返す
+
+    Args:
+        video (_type_): _description_
+
+    Returns:
+        x0[int], y0[int], w[int], h[int]: _description_
+    """
 
     x0, y0, w, h = cv2.selectROI(frame)
     if x0 is None or y0 is None or w is None or h is None:
         print("トリミング領域の指定に失敗しました。")
         exit()
-
+    cv2.destroyAllWindows()
     return x0, y0, w, h
 
 
-# インストール済みのTesseractのパスを通す
-TESSERACT_PATH = "C:\Program Files\Tesseract-OCR"
-if TESSERACT_PATH not in os.environ["PATH"].split(os.pathsep):
-    os.environ["PATH"] += TESSERACT_PATH
+def main():
 
-# ファイルの指定
-video_path = 'data\sample2.mp4'
-video = cv2.VideoCapture(video_path)
+    # 定数宣言
+    START_FRAME = 120    # 最初の数秒を飛ばす時間
+    FRAME_INTERVAL = 10 # 画像を読み取る間隔
+    TEST_FLAG = True    # テスト用の定数
 
-# 動画のフレーム数を取得する
-frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    # 変数宣言
+    prev_name = ""
+    prev_text = ""
 
-# 1フレーム前のテキストを格納する変数の宣言
-prev_text = ""
-prev_name = ""
+    # 初期化
+    Init()
 
-x0, y0, w, h = CaptureArea(video)
+    # 動画の取り込み
+    video = cv2.VideoCapture('data\sample1.mp4')
 
-# 定数指定
-TEXT_X = x0     # 245
-TEXT_Y = y0     # 497
-TEXT_W = w      # 787
-TEXT_H = h      # 114
-NAME_X = 252
-NAME_Y = 454
-NAME_W = 125
-NAME_H = 34
-TEST_FLAG = True
+    # 動画の総フレーム数を取得する
+    frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
+    # トリミングのために2秒進める
+    for i in range(START_FRAME):
+        ret, frame = video.read(i)
 
-# 60フレームごとに処理を繰り返す
-for i in range(0, frame_count):
+    # 各ウィンドウの領域を選択
+    name_x, name_y, name_w, name_h = CaptureArea(frame)
+    text_x, text_y, text_w, text_h = CaptureArea(frame)
 
-    # フレームを取得する
-    ret, frame = video.read(i)
+    # 動画の読み取り処理
+    for i in range(START_FRAME, frame_count):
 
-    # 一定フレームのときのみ動作
-    if i % 10 != 0 and i != frame_count - 1:
-        continue
+        # フレームを取得する
+        ret, frame = video.read(i)
 
-    if ret:
+        # 一定フレーム間隔で読み取りを行う
+        if i % FRAME_INTERVAL != 0 and i != frame_count - 1:
+            continue
+
         # 画像表示(テスト用)
         if TEST_FLAG:
             cv2.imshow("frame", frame)
 
-        # トリミング
-        text_roi = frame[TEXT_Y:TEXT_Y + TEXT_H, TEXT_X:TEXT_X + TEXT_W]
+        # 読み取りの可否
+        if ret:
 
-        # OCR を使用して、画像からテキストを取得する
-        cur_text = pytesseract.image_to_string(text_roi, lang="jpn")
+            # テキスト部分を画像認識
+            text_roi = frame[text_y:text_y + text_h, text_x:text_x + text_w]
+            cur_text = pytesseract.image_to_string(text_roi, lang="jpn", config="--oem 1")
+            cur_text = cur_text.replace(" ", "")
 
-        # テキスト処理
-        cur_text = cur_text.replace(" ", "")
+            # 前のセリフより短くなったタイミングで書き出し処理
+            if len(cur_text) < len(prev_text) - 1:  # 画像認識の揺れでテキストが短くなる場合を考慮して-1
 
-        # 前のセリフより短くなったタイミングで書き出し処理
-        if len(cur_text) < len(prev_text) - 1:  # 画像認識の揺れでテキストが短くなる場合を考慮して-1
+                text = f'{prev_name}:{prev_text}\n'
+                with open("output.txt", "a", encoding="utf-8") as f:
+                    f.write(text)
 
-            # テキスト
-            prev_name = prev_name.strip("\n")
-            text = prev_text
-            # text = f'{prev_name}:{prev_text}\n'
+                # テキスト出力(テスト用)
+                if TEST_FLAG:
+                    print(i, text)
 
-            # テキスト出力(テスト用)
+            # 1フレーム前のテキストを保持
+            prev_text = cur_text
+
+            # キャラ名部分を画像認識
+            name = frame[name_y:name_y + name_h, name_x:name_x + name_w]
+            name = cv2.cvtColor(name, cv2.COLOR_BGR2GRAY)
+            cv2.threshold(name, 200, 255, cv2.THRESH_BINARY, dst=name)
+            name = cv2.bitwise_not(name)
             if TEST_FLAG:
-                print(i, text)
+                cv2.imshow("name", name)
+            prev_name = pytesseract.image_to_string(name, lang="jpn", config="--psm 4, --oem 1")
+        else:
+            print('読み込めませんでした。')
 
-            # テキスト保存
-            with open("output.txt", "a", encoding="utf-8") as f:
-                f.write(text)
-
-        prev_text = cur_text
-        name_roi = frame[NAME_Y:NAME_Y + NAME_H, NAME_X:NAME_X + NAME_W]
-        prev_name = pytesseract.image_to_string(name_roi, lang="jpn")
-
-    else:
-        print('読み込めませんでした。')
-
-    # キーボード入力を待つ
-    key = cv2.waitKey(100)
-
-    # スペースキーが押されたら終了する
-    if key == 32:
-        break
+        # キーボード入力を待つ 
+        key = cv2.waitKey(100)
+        # スペースキーが押されたら終了する
+        if key == 32:
+            break
 
 
-
+if __name__ == "__main__":
+    main()
