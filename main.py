28cb7b8565182ba4a8113b41f1f82b6a309572a1
ShunsukeTamaki <tamazoo0913@gmail.com>
2023-11-26 20:42:43 +0900

    update/main.py 画像拡大化処理の追加

--- main.py
+++ main.py
@@ -34,13 +34,14 @@ def CaptureArea(frame):
 def main():
 
     # 定数宣言
-    START_FRAME = 120    # 最初の数秒を飛ばす時間
+    START_FRAME = 2 * 60    # 最初の数秒を飛ばす時間
     FRAME_INTERVAL = 10 # 画像を読み取る間隔
     TEST_FLAG = True    # テスト用の定数
 
     # 変数宣言
     prev_name = ""
     prev_text = ""
+    text = ""
 
     # 初期化
     Init()
@@ -77,18 +78,20 @@ def main():
         if ret:
 
             # テキスト部分を画像認識
-            text_roi = frame[text_y:text_y + text_h, text_x:text_x + text_w]
-            cur_text = pytesseract.image_to_string(text_roi, lang="jpn", config="--psm 3,--oem 1")
+            text_roi = frame[text_y:text_y + text_h, text_x:text_x + text_w] # TODO:関数化
+            text_roi = cv2.cvtColor(text_roi, cv2.COLOR_BGR2GRAY)
+            cv2.threshold(text_roi, 200, 255, cv2.THRESH_BINARY, dst=text_roi)
+            h, w = text_roi.shape[:2]
+            text_roi = cv2.resize(text_roi, dsize=(w*2, h*2))
+            if TEST_FLAG:
+                cv2.imshow("text_roi", text_roi)
+            cur_text = pytesseract.image_to_string(text_roi, lang="jpn", config="--psm 6, --oem 3")
             cur_text = cur_text.replace(" ", "")
 
             # 前のセリフより短くなったタイミングで書き出し処理
             if len(cur_text) < len(prev_text) - 2:  # 画像認識の揺れでテキストが短くなる場合を考慮して-2
 
-                text = f'{prev_name}{prev_text}\n'
-                with open("output.txt", "a", encoding="utf-8") as f:
-                    f.write(text)
-
-                # テキスト出力(テスト用)
+                text += f'{prev_name}{prev_text}\n'
                 if TEST_FLAG:
                     print(text)
 
@@ -96,21 +99,30 @@ def main():
             prev_text = cur_text
 
             # キャラ名部分を画像認識
-            name = frame[name_y:name_y + name_h, name_x:name_x + name_w]
+            name = frame[name_y:name_y + name_h, name_x:name_x + name_w]    # TODO:関数化
             name = cv2.cvtColor(name, cv2.COLOR_BGR2GRAY)
             cv2.threshold(name, 200, 255, cv2.THRESH_BINARY, dst=name)
             name = cv2.bitwise_not(name)
+
+            # nameのサイズを6倍
+            h, w = name.shape[:2]
+            name = cv2.resize(name, dsize=(w*6, h*6))
             if TEST_FLAG:
                 cv2.imshow("name", name)
-            prev_name = pytesseract.image_to_string(name, lang="jpn", config="--psm 8, --oem 1")
+            prev_name = pytesseract.image_to_string(name, lang="jpn", config="--psm 8, --oem 3, name_config")
+            prev_name = prev_name.replace(" ", "")
         else:
             print('読み込めませんでした。')
 
-        # キーボード入力を待つ
-        key = cv2.waitKey(100)
         # スペースキーが押されたら終了する
+        key = cv2.waitKey(100)
         if key == 32:
+            with open("output.txt", "a", encoding="utf-8") as f:
+                f.write(text)
             break
+
+    with open("output.txt", "a", encoding="utf-8") as f:
+        f.write(text)
 
 
 if __name__ == "__main__":
