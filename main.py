163501043edc53a590a483001eb274a5d7f55534
ShunsukeTamaki <tamazoo0913@gmail.com>
2023-11-28 00:29:58 +0900

    update/main.py 関数化リファクタ

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ import numpy as np
 from difflib import SequenceMatcher
 
 
-def Init() -> None:
-    """インストール済みのTesseractのパスを通す
-    """
-    
-    TESSERACT_PATH = "C:\Program Files\Tesseract-OCR"
+START_FRAME = 2 * 60    # 最初の数秒を飛ばす時間
+FRAME_INTERVAL = 10  # 画像を読み取る間隔
+TEST_FLAG = True    # テストフラグ
+TESSERACT_PATH = "C:\Program Files\Tesseract-OCR" # 環境変数のパス
+
+
+def SetPath() -> None:
+    """環境変数にパスを通す"""
+
     if TESSERACT_PATH not in os.environ["PATH"].split(os.pathsep):
         os.environ["PATH"] += TESSERACT_PATH
 
 
-def CaptureArea(frame):
+def CaptureArea(frame: np.ndarray) -> (int, int, int, int):
     """指定した領域の座標と幅と高さを返す
 
     Args:
@@ -32,12 +36,39 @@ def CaptureArea(frame):
     return x0, y0, w, h
 
 
-def main():
+def OcrImage(img: np.ndarray, scale: int, mode: int, reverse_flag: bool = False) -> str:
+    """画像から文字を読み取る
 
-    # 定数宣言
-    START_FRAME = 2 * 60    # 最初の数秒を飛ばす時間
-    FRAME_INTERVAL = 10 # 画像を読み取る間隔
-    TEST_FLAG = True    # テスト用の定数
+    Args:
+        img (np.ndarray): 画像データ
+        scale (int): 画像の拡大倍率
+        mode (int): tesseractのpsmモードの選択
+        reverse_flag (bool): ビット反転処理フラグ
+
+    Returns:
+        str: 読み取った文字列
+    """
+
+    # グレースケール変換
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    threshold = 200
+    cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY, dst=img)
+
+    # ビット反転処理
+    if reverse_flag:
+        img = cv2.bitwise_not(img)
+
+    # 拡大処理
+    h, w = img.shape[:2]
+    img = cv2.resize(img, dsize=(w * scale, h * scale))
+
+    # 画像認識
+    text = pytesseract.image_to_string(img, lang="jpn", config=f"--psm {mode}, --oem 3, name_config")
+
+    return text.replace(" ", "")
+
+
+def main():
 
     # 変数宣言
     prev_name = ""
@@ -45,7 +76,7 @@ def main():
     text = ""
 
     # 初期化
-    Init()
+    SetPath()
 
     # 動画の取り込み
     video = cv2.VideoCapture('data\sample1.mp4')
@@ -62,7 +93,7 @@ def main():
     text_x, text_y, text_w, text_h = CaptureArea(frame)
 
     # 動画の読み取り処理
-    for i in range(START_FRAME, frame_count):
+    for i in range(START_FRAME, frame_count): # TODO:ここでFRAME_INTERVAL刻みにすればよくね？
 
         # フレームを取得する
         ret, frame = video.read(i)
@@ -71,7 +102,7 @@ def main():
         if i % FRAME_INTERVAL != 0 and i != frame_count - 1:
             continue
 
-        # 画像表示(テスト用)
+        # 画像表示
         if TEST_FLAG:
             cv2.imshow("frame", frame)
 
@@ -79,15 +110,8 @@ def main():
         if ret:
 
             # テキスト部分を画像認識
-            text_roi = frame[text_y:text_y + text_h, text_x:text_x + text_w] # TODO:関数化
-            text_roi = cv2.cvtColor(text_roi, cv2.COLOR_BGR2GRAY)
-            cv2.threshold(text_roi, 200, 255, cv2.THRESH_BINARY, dst=text_roi)
-            h, w = text_roi.shape[:2]
-            text_roi = cv2.resize(text_roi, dsize=(w*2, h*2))
-            if TEST_FLAG:
-                cv2.imshow("text_roi", text_roi)
-            cur_text = pytesseract.image_to_string(text_roi, lang="jpn", config="--psm 6, --oem 3")
-            cur_text = cur_text.replace(" ", "")
+            text_img = frame[text_y:text_y + text_h, text_x:text_x + text_w]
+            cur_text = OcrImage(text_img, 2, 6)
 
             # 前のセリフとの一致率が低くなったタイミングで書き出し処理
             if SequenceMatcher(None, cur_text, prev_text).ratio() < 0.3:
@@ -98,27 +122,15 @@ def main():
             # 1フレーム前のテキストを保持
             prev_text = cur_text
 
-            # キャラ名部分を画像認識
-            name = frame[name_y:name_y + name_h, name_x:name_x + name_w]    # TODO:関数化
-            name = cv2.cvtColor(name, cv2.COLOR_BGR2GRAY)
-            cv2.threshold(name, 200, 255, cv2.THRESH_BINARY, dst=name)
-            name = cv2.bitwise_not(name)
-
-            # nameのサイズを6倍
-            h, w = name.shape[:2]
-            name = cv2.resize(name, dsize=(w*6, h*6))
-            if TEST_FLAG:
-                cv2.imshow("name", name)
-            prev_name = pytesseract.image_to_string(name, lang="jpn", config="--psm 8, --oem 3, name_config")
-            prev_name = prev_name.replace(" ", "")
+            # キャラ名を画像認識
+            name_img = frame[name_y:name_y + name_h, name_x:name_x + name_w]
+            prev_name = OcrImage(name_img, 6, 8, True)
         else:
             print('読み込めませんでした。')
 
         # スペースキーが押されたら終了する
         key = cv2.waitKey(100)
         if key == 32:
-            with open("output.txt", "a", encoding="utf-8") as f:
-                f.write(text)
             break
 
     with open("output.txt", "a", encoding="utf-8") as f:
